d8f98e1eab2173ed1147b21e0bc1deedaa5797a7
mJaJksJ <maksim.m00@mail.ru>
2023-05-15 21:58:21 +0300

    chore: лаб-4 доработки

--- labs/lab4.py
+++ labs/lab4.py
@@ -1,3 +1,5 @@
+from matplotlib import pyplot as plt
+
 import cl_processor as clp
 import numpy as np
 from image_processor import process_buffer
@@ -69,8 +71,8 @@ to_grayscale= {
 }
 
 
-def by_buffers():
-    image = cv2.imread("image.jpg", cv2.IMREAD_COLOR)
+def by_buffers(image_name):
+    image = cv2.imread(image_name, cv2.IMREAD_COLOR)
     data = np.asarray(image)
     box_blur_code = f"""
         {box_blur['defines']}
@@ -82,7 +84,7 @@ def by_buffers():
           FOREACH_PIXEL(PIXEL_INDEX, GET_COLOR_INDEX(BLUE, blur_row, PIXEL_INDEX)  = CREATE_SUM_NAME(BLUE))
         }}
     """
-    blur, box_blur_time = process_buffer(box_blur_code, data, box_blur['kernel'], np.int32(3))
+    blur, box_blur_time = process_buffer(box_blur_code, data, box_blur['kernel'], np.int32(5))
 
     grayscale_code = f"""
         {to_grayscale['signature']}
@@ -92,12 +94,12 @@ def by_buffers():
     """
     grayscale, grayscale_time = process_buffer(grayscale_code, blur, to_grayscale['kernel'])
 
-    cv2.imwrite("by_buffer.jpg", grayscale)
+    cv2.imwrite("by_buffer_"+image_name, grayscale)
     return box_blur_time + grayscale_time
 
 
-def by___pipes():
-    image = cv2.imread("image.jpg", cv2.IMREAD_COLOR)
+def by___pipes(image_name):
+    image = cv2.imread(image_name, cv2.IMREAD_COLOR)
     data = np.asarray(image)
     blur_and_grey_text = f"""
         #define SPIN_READ_PIPE(PIPE, DEST) if(read_pipe(PIPE, &DEST)){{mem_fence(CLK_GLOBAL_MEM_FENCE);}};
@@ -145,21 +147,34 @@ def by___pipes():
         }}
         """
     ctx, queue = clp.create_context_and_queue()
-    pipe = clp.get_rw_pipe(ctx, 3, 4096)
+    pipe = clp.get_rw_pipe(ctx, 1024, 66000)
     blurred_and_grey_data, blurred_and_grey_time = process_buffer(blur_and_grey_text,
                                                                   data,
                                                                   [box_blur["kernel"],
                                                                    to_grayscale["kernel"]],
-                                                                  np.int32(3),
+                                                                  np.int32(5),
                                                                   pipe,
                                                                   pipe,
                                                                   multiple=True,
                                                                   options=['-cl-std=CL2.0'],
                                                                   ctx=ctx,
                                                                   queue=queue)
-    cv2.imwrite("by_pipe.jpg", blurred_and_grey_data)
+    cv2.imwrite("by_pipe_"+image_name, blurred_and_grey_data)
     return blurred_and_grey_time
 
 
-ic(by_buffers())
-ic(by___pipes())
+szs = [32, 64, 128, 256, 512, 720]
+
+by_bufs = []
+by_pipes = []
+for sz in szs:
+    nm = f"image{sz}.jpg"
+    by_bufs.append(by_buffers(nm))
+    by_pipes.append(by___pipes(nm))
+    ic(sz, by_bufs[-1], by_pipes[-1])
+
+fig, ax = plt.subplots()
+ax.plot(szs, by_bufs, label='by bufs')
+ax.plot(szs, by_pipes, label='by pipes')
+plt.legend()
+plt.show()
